1013736e1f01927921233be71021e09a75f798a4
Ekaterina Kutovaya <e.katerina.kutovaya@yandex.ru>
2022-10-27 13:48:18 +0500

    routes

--- index.js
+++ index.js
@@ -17,14 +17,17 @@ app.use(express.static('client/build'));
 app.use('/api', router);
 app.get('/', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
 app.get('/dashboard/companies', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
+app.get('/dashboard/employees', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
+app.get('/dashboard/orders', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
+app.get('/dashboard/company_details/:id', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
 app.use(errorHandler);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'client/build')))
-    app.get('/dashboard/companies', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
-    app.use('/api', router);
-    app.use(errorHandler);
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, 'client/build')))
+//     app.get('/dashboard/companies', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
+//     app.use('/api', router);
+//     app.use(errorHandler);
+// }
 
 
 const start = async () => {
