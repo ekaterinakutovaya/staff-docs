34bbf723d570fd62a31cbb31b8d69a89c29341e0
Ekaterina Kutovaya <e.katerina.kutovaya@yandex.ru>
2022-10-27 12:22:28 +0500

    database

--- db.js
+++ db.js
@@ -1,24 +1,18 @@
 const {Sequelize} = require('sequelize');
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT
+//     }
+// )
+
+module.exports = new Sequelize('postgresql://postgres:dZkKiT8gaM82OMb3ds3V@containers-us-west-95.railway.app:7931/railway', {
+    define: {
+        timestamps: true
     }
-)
-
-// const Pool = require("pg").Pool;
-
-// const pool = new Pool({
-//     user: "postgres",
-//     password: "123",
-//     host: "localhost",
-//     database: "staff",
-//     port: 5432
-// })
-
-// module.exports = pool;
+})
