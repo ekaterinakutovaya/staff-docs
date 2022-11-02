88036d3a8498d2a407f4d7d0318443e04e298cdb
Ekaterina Kutovaya <e.katerina.kutovaya@yandex.ru>
2022-11-02 14:24:24 +0500

    professions table

--- controllers/professionController.js
+++ controllers/professionController.js
@@ -3,38 +3,30 @@ const path = require('path');
 const { Op } = require("sequelize");
 const { Profession } = require('../models/models');
 const ApiError = require('../error/ApiError');
-// const sequelize = require('sequelize');
-const Sequelize = require('../db');
 const fs = require('fs');
-const parse = require('csv-parse')
-const CsvReadableStream = require('csv-reader');
-const dbMod = require('csv-sequelize');
+
 
 class ProfessionController {
 
     async create(req, res) {
-        const myPath = path.resolve(__dirname, 'prof.csv');
-        const jobs = await Sequelize.query(`COPY professions FROM ${myPath} DELIMITER ';' CSV HEADER ENCODING 'windows-1251'`, {
-            model: Profession
-        })
-        console.log(myPath);
+        const file = path.resolve(__dirname, './data.json')
+        
+        fs.readFile(file, 'utf8', async function (err, data) {
+            if (err) throw err;
+            const obj = JSON.parse(data);
 
-
-
-        // const data = [];
-        // let inputStream = fs.createReadStream('../prof.csv', 'utf-8');
-
-        // inputStream
-        //     .pipe(new CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true }))
-        //     .on('data', function (row) {
-        //         // console.log('Row: ', row);
-        //         data.push(row);
-        //     })
-        //     .on('end', function () {
-        //         console.log(data);
-        //     });
-
-
+            obj.map(item => {
+              
+                const job = Profession.create({
+                   id: item.id,
+                   profession: item.profession,
+                   createdAt: item.createdAt,
+                   updatedAt: item.updatedAt
+                });
+                
+            })
+     
+        });
 
     }
 
